d7cd9c8d5b39d0361da7b8766187d6953589663c
Drew Lovato <gaethlovato@gmail.com>
2022-09-11 17:54:13 -0600

    schema is currently working when running through terminal

--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,24 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE departments (
-    id INT NOT NULL PRIMARY KEY,
-    departments_name VARCHAR(30) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL PRIMARY KEY,
-    title VARCHAR(30),
-    salary INT,
-    departments_name VARCHAR(30),
-    FOREIGN KEY (departments_name)
-    REFERENCES departments(departments_name)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary INT NOT NULL,
+    departments_id INT NOT NULL,
+    FOREIGN KEY (departments_id) REFERENCES departments(id)
 );
 
--- CREATE TABLE employee (
---     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     first_name VARCHAR(30),
---     last_name VARCHAR(30),
---     role_id INT,
---     manager_id INT
--- );
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
